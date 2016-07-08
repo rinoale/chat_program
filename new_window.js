1ea0d3a4834a76f9dfd858ca97f148520fd53837
rinoale <rinoale@hanmail.net>
2016-07-08 04:44:37 +0100

    mod css width and height. add bottom area

--- new_window.js
+++ new_window.js
@@ -5,7 +5,7 @@ const newWindowBtn = document.getElementById('new-window')
 
 newWindowBtn.addEventListener('click', function (event) {
   const modalPath = path.join('file://', __dirname, 'resources/foundation/index.html')
-  let win = new BrowserWindow({ width: 400, height: 320 })
+  let win = new BrowserWindow({ width: 800, height: 600 })
   win.on('close', function () { win = null })
   win.loadURL(modalPath)
   win.show()
